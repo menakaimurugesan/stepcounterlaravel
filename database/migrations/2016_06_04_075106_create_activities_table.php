b0a02be573e5d90f9a089e003cf2af71663a3f94
menakaimurugesan <menakai@gmail.com>
2016-06-15 10:42:41 +0530

    Error correction on migration file
    File was corrupted with the issues in commit and its rectified.

--- database/migrations/2016_06_04_075106_create_activities_table.php
+++ database/migrations/2016_06_04_075106_create_activities_table.php
@@ -14,21 +14,13 @@ class CreateActivitiesTable extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->increments('id');
-<<<<<<< HEAD
-			$table->integer('user_id')->unsigned()->index();
-=======
-			$table->integer('user_id')->unsigned();
->>>>>>> origin/master
+			$table->integer('user_id')->unsigned()->index();			
             $table->date('date');
 			$table->integer('steps');
             $table->timestamps();			
 		});
 		Schema::table('activities', function($table) {
-<<<<<<< HEAD
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-=======
-			$table->foreign('user_id')->references('id')->on('users');
->>>>>>> origin/master
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');			
 		});
     }
 
